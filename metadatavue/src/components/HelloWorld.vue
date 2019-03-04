dcd40366506039928f4193c01b7f15e0152b6382
Sondre <holtsdalen@hotmail.com>
2019-03-04 10:25:12 +0100

    Initial commit

---
+++ metadatavue/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <super-form v-bind:metadata="mySuperform.metadata" v-bind:data="mySuperform.data" />
+    <hr>
+
+  </div>
+</template>
+
+<script>
+import Superform from '@/components/Superform';
+
+export default {
+  data() {
+    return {
+      mySuperform: {
+        metadata: {
+          columns: ["name", "birthyear", "sex"]
+        },
+        data: {
+          name: "Terje",
+          birthyear: 1975,
+          sex: "male",       
+        }
+      }
+    }
+  },
+  components: {
+    SuperForm: Superform
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
---
+++ metadatavue/src/components/Superform.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <table v-for="column in columns">
+        <tr>
+            <th></th>
+            <th></th>
+            <th></th>          
+        </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() { 
+    return {};
+  },
+  name: "Superform",
+  methods:{
+    
+  },
+};
+</script>
+
+<style>
+</style>
